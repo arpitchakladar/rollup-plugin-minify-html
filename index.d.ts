0819b7a73b11cf2155a10e6a6bf80c428c9ac2c9
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-03-27 19:30:43 +0530

    Added the option to minify all output html in the final bundle.
    Using NPM instead of Yarn.

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,11 @@
 import type { Plugin } from "rollup";
 import type { Options as HTMLMinifierOptions } from "html-minifier";
 
-interface RollupPluginMinifyHTMLTargetOptions {
-	src: string;
-	dest: string;
-	minifierOptions: HTMLMinifierOptions
-};
-
 interface RollupPluginMinifyHTMLOptions {
 	targets?: RollupPluginMinifyHTMLTargetOptions[],
 	hook?: string;
-	minifierOptions?: HTMLMinifierOptions
+	minifierOptions?: HTMLMinifierOptions;
+	minifyOutput?: boolean;
 };
 
 export const minifyHTML = (options?: RollupPluginMinifyHTMLOptions) => Plugin;
